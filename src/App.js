a6c5920110bef862aab9104ca1bd68a9865fb22e
kouWeb101 <81058523+kouWeb101@users.noreply.github.com>
2021-11-03 19:52:45 +0900

    基本的なルーティング

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      {/* ルーティングする際は全体を囲む */}
+      <div className="App">
+        {/* aタグのような扱いで遷移させることが出来る 
+        to="XXXX"がhref属性に相当する
+    */}
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      {/* 
+      ルーティングの設定
+      <Switch>
+      exactを付けないと完全一致ではないので/(ルート)配下も遷移の対象にしてしまう
+        <Route exact path="/">
+        </Route>
+      </Switch>
+    */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
