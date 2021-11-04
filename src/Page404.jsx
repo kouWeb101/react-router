bbac82633b4482857609a3e604297fe40c0f9f2b
kouWeb101 <81058523+kouWeb101@users.noreply.github.com>
2021-11-04 12:14:37 +0900

    ルート定義の分割、url、クエリパラメータの受け取り、404ページの実装

---
+++ src/Page404.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const Page4040 = () => {
+  return (
+    <div>
+      <h1>ページが見つかりません</h1>
+      <Link to="/">ホームページへ戻る</Link>
+    </div>
+  );
+};
